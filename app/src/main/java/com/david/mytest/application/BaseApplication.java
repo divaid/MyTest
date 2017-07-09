61551e66f7b74b3ef8da983198da441603031bdb
cuiweixing <m15001105461@163.com>
2017-07-09 18:04:52 +0800

    application补提交

--- app/src/main/java/com/david/mytest/application/BaseApplication.java
+++ app/src/main/java/com/david/mytest/application/BaseApplication.java
@@ -1,7 +1,6 @@
 package com.david.mytest.application;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
@@ -10,25 +9,15 @@ import com.squareup.leakcanary.RefWatcher;
  * Created by david on 2016/7/1.
  */
 public class BaseApplication extends Application {
-    private static Context sContext;
+    public static BaseApplication mApplication;
 
     public static RefWatcher mWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        sContext = this;
+        mApplication = this;
 
         mWatcher = LeakCanary.install(this);
-
-        
-    }
-
-    /**
-     * 对外提供Application的Context
-     * @return
-     */
-    public static Context getApplication(){
-        return sContext;
     }
 }
