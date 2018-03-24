55c413d04a11306bc46f3243c30e963ef74b91d6
david <m15001105461@163.com>
2018-03-24 16:52:06 +0800

    添加文件

---
+++ app/src/main/java/com/david/mytest/activity/mine/LocationTestActivity.java
@@ -0,0 +1,15 @@
+package com.david.mytest.activity.mine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by weixi on 2017/8/2.
+ */
+
+public class LocationTestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/david/mytest/activity/search/SearchActivity.java
@@ -0,0 +1,18 @@
+package com.david.mytest.activity.search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.david.mytest.R;
+
+/**
+ * Created by weixi on 2017/7/6.
+ */
+
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
